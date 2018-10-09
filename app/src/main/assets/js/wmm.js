5b1d4c19a0292b2d5a2e3a5bcba5b2a6a3a79c37
wuminmin <13355661100@189.cn>
2018-10-09 14:39:36 +0800

    1

--- app/src/main/assets/js/wmm.js
+++ app/src/main/assets/js/wmm.js
@@ -33,14 +33,74 @@ $(document).ready(function () {
     console.log(jianzu)
     if( dizhi === ''){
     }else{
-        $("#dizhi").val( decodeURIComponent(dizhi) );
+        $("#shineidizhi").val( decodeURIComponent(dizhi) );
     }
 
     if(jianzu == ''){
     }else{
         jianzu = jianzu.replace("详情»", "")
-        $("#jianzu").val( decodeURIComponent( jianzu) );
+        $("#shineijianzu").val( decodeURIComponent( jianzu) );
     }
+
+    
+    $('.demo1').click(function () {
+                swal({
+                    title: "欢迎使用SweetAlert",
+                    text: "Sweet Alert 是一个替代传统的 JavaScript Alert 的漂亮提示效果。"
+                });
+            });
+
+    $('.demo2').click(function () {
+        swal({
+            title: "太帅了",
+            text: "小手一抖就打开了一个框",
+            type: "success"
+        });
+    });
+
+    $('.demo3').click(function () {
+        swal({
+            title: "您确定要删除这条信息吗",
+            text: "删除后将无法恢复，请谨慎操作！",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "删除",
+            closeOnConfirm: false
+        }, function () {
+            swal("删除成功！", "您已经永久删除了这条信息。", "success");
+        });
+    });
+
+    $('.demo4').click(function () {
+        swal({
+                title: "您确定要上传室内场景采集数据吗？",
+                text: "请在上传前，检查数据是否完整",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "是的，我要上传！",
+                cancelButtonText: "让我再考虑一下…",
+                closeOnConfirm: false,
+                closeOnCancel: false
+            },
+            function (isConfirm) {
+                if (isConfirm) {
+                    Android.deletePhoto();
+
+                          var a = "  <div class=\"item active\">\n" +
+            "                        <img alt=\"image\" class=\"img-responsive\" src=\"file:///android_asset/img/hehua.jpg\">\n" +
+            "                    </div>";
+                     document.getElementById("myImage").innerHTML = a;
+                    swal("上传成功！", "您已经上传室内场景采集数据。", "success");
+
+                } else {
+                    swal("已取消", "您取消了操作！", "error");
+                }
+            });
+    });
+
+
 });
 
 
